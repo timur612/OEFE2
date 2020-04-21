d6942aa9dc6e8c2fa992680c4ad4e958791394a8
timur612 <tz220404@gmail.com>
2020-04-21 18:00:16 +0900

    drag

--- app/src/main/java/com/timur/itlab/oefe/StudyActivity.kt
+++ app/src/main/java/com/timur/itlab/oefe/StudyActivity.kt
@@ -21,10 +21,7 @@ import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 
 
 
-
-
-
-class StudyActivity : AppCompatActivity(), View.OnTouchListener, View.OnDragListener {
+class StudyActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,90 +36,15 @@ class StudyActivity : AppCompatActivity(), View.OnTouchListener, View.OnDragList
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
 
-        setListeners()
+
     }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
 
-    private fun setListeners() {
-        tv_dropdrop.setOnTouchListener(this)
-        ll_pinklayout.setOnDragListener(this)
-        drag_ans.setOnDragListener(this)
-    }
-
-    override fun onDrag(view: View, dragEvent: DragEvent):Boolean {
-        var dragAction = dragEvent.getAction()
-        Log.d(TAG, "onDrag: view->$view\n DragEvent$dragEvent")
-        when (dragEvent.action) {
-            DragEvent.ACTION_DRAG_ENDED -> {
-                Log.d(TAG, "onDrag: ACTION_DRAG_ENDED ")
-                val result = dragEvent.getResult()
-                if(result)
-                {
-                    Log.d(TAG, "Succeded")
-                }else
-                {
-                    Log.d(TAG, "Failed")
-                }
-                return true
-            }
-            DragEvent.ACTION_DRAG_EXITED -> {
-                Log.d(TAG, "onDrag: ACTION_DRAG_EXITED")
-                return true
-            }
-            DragEvent.ACTION_DRAG_ENTERED -> {
-                Log.d(TAG, "onDrag: ACTION_DRAG_ENTERED")
-                return true
-            }
-            DragEvent.ACTION_DRAG_STARTED -> {
-                Log.d(TAG, "onDrag: ACTION_DRAG_STARTED")
-                var clipDescription = dragEvent.getClipDescription()
-                return true
-            }
-            DragEvent.ACTION_DROP -> {
-                val clipData = dragEvent.getClipData()
-                val itemCount = clipData.getItemCount()
-
-                if(itemCount > 0){
-                    val item = clipData.getItemAt(0)
-                    val dragDropString = item.getText().toString()
-                    val srcView = dragEvent.localState as View
-                    val owner = srcView.parent as ViewGroup
-                    owner.removeView(srcView)
-                    val newContainer = view as LinearLayout
-                    newContainer.addView(srcView)
-                    srcView.setVisibility(View.VISIBLE)
-                    // Returns true to make DragEvent.getResult() value to true.
-                    return true
-                }else if(dragAction == DragEvent.ACTION_DRAG_LOCATION)
-                {
-                    return true
-                }else
-                {
-                   Log.d(TAG,"что-то не так")
-                }
 
 
 
-                return false
-            }
-            DragEvent.ACTION_DRAG_LOCATION -> {
-                Log.d(TAG, "onDrag: ACTION_DRAG_LOCATION")
-                return true
-            }
-            else -> return false
-        }
-    }
-     override fun onTouch(view:View, motionEvent: MotionEvent):Boolean {
-        Log.d(TAG, "onTouch: view->view$view\n MotionEvent$motionEvent")
-        return if (motionEvent.action === MotionEvent.ACTION_DOWN) {
-            val dragShadowBuilder = View.DragShadowBuilder(view)
-            view.startDrag(null, dragShadowBuilder, view, 0)
-            true
-        } else {
-            false
-        }
-    }
+
 }
--- app/src/main/java/com/timur/itlab/oefe/activity_conf.kt
+++ app/src/main/java/com/timur/itlab/oefe/activity_conf.kt
@@ -2,11 +2,49 @@ package com.timur.itlab.oefe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import com.vidyo.VidyoClient.Connector.Connector
+import com.vidyo.VidyoClient.Connector.ConnectorPkg
 
-class activity_conf : AppCompatActivity() {
+class activity_conf : AppCompatActivity(), Connector.IConnect {
 
+
+    lateinit var vc:Connector
+    lateinit var videoFrame:FrameLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conf)
+
+        ConnectorPkg.setApplicationUIContext(this)
+        ConnectorPkg.initialize()
+
+        videoFrame = findViewById(R.id.videoFrame)
+
+
+    }
+
+    fun Start(v: View){
+        vc = Connector(videoFrame,Connector.ConnectorViewStyle.VIDYO_CONNECTORVIEWSTYLE_Default,16,"","",0)
+        vc.showViewAt(videoFrame,0,0,videoFrame.width,videoFrame.height)
+    }
+    fun Connect(v: View){
+        val token:String = "cHJvdmlzaW9uAHVzZXIxQDc2ZWY4NC52aWR5by5pbwA2Mzc1NDY3OTU0NwAAYjM5YTNiZThmZThiNTczNGMzMWY3OTdhOTU0ZmNjYjgyZTU5OWUwOGJmNjFhOGYyMzQ4MGJhZjIwYzRmY2I5NDc0ZGNjNmEwYWNiZmVjMGM4NjQ3NGVjOTA5MmJmMTQw"
+        vc.connect("prod.vidyo.io",token,"DemoUser","DemoRoom",this)
+
+    }
+    fun Disconnect(v:View){
+        vc.disconnect()
+    }
+    override fun onDisconnected(p0: Connector.ConnectorDisconnectReason?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onFailure(p0: Connector.ConnectorFailReason?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onSuccess() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
