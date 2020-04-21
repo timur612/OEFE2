139170e9b3a6b5359cb200b710d73360fe576222
timur612 <tz220404@gmail.com>
2020-04-21 10:52:10 +0900

    заккоментил и поменял метсами

--- app/src/main/java/com/timur/itlab/oefe/StudyActivity.kt
+++ app/src/main/java/com/timur/itlab/oefe/StudyActivity.kt
@@ -8,5 +8,17 @@ class StudyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study)
+
+        //actionbar
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Задания"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
