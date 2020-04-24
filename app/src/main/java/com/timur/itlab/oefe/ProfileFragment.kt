ff2d5650035b9989f24be7b44bade3536c21c3a2
timur612 <tz220404@gmail.com>
2020-04-24 13:11:19 +0900

    последняя

--- app/src/main/java/com/timur/itlab/oefe/ProfileFragment.kt
+++ app/src/main/java/com/timur/itlab/oefe/ProfileFragment.kt
@@ -4,10 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import androidx.cardview.widget.CardView
 import kotlinx.android.synthetic.main.dostij_include_profile.*
 import kotlinx.android.synthetic.main.dostij_include_profile.view.*
 import kotlinx.android.synthetic.main.fragment_profile.*
@@ -15,19 +16,24 @@ import kotlinx.android.synthetic.main.fragment_profile.view.*
 import kotlinx.android.synthetic.main.study_fragment.view.*
 
 class ProfileFragment: Fragment(){
-
+    val MENU_APLHA_ID = 1
+    val MENU_SCALE_ID = 2
+    val MENU_TRANSLATE_ID = 3
+    val MENU_ROTATE_ID = 4
+    val MENU_COMBO_ID = 5
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
 
-        val view: View = inflater!!.inflate(R.layout.fragment_profile, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_profile, container, false)
 
-        view.textView14.setOnClickListener{view ->
+
+        view.textView14.setOnClickListener{
             dostij_card.visibility = View.VISIBLE
             cardView.visibility = View.GONE
-            bbb.alpha
+
         }
-        view.close_btn.setOnClickListener{view->
+        view.close_btn.setOnClickListener{
             dostij_card.visibility = View.GONE
             cardView.visibility = View.VISIBLE
         }
@@ -38,4 +44,22 @@ class ProfileFragment: Fragment(){
     companion object {
         fun newInstance(): ProfileFragment = ProfileFragment()
     }
+    /* override fun onCreateContextMenu(menu:ContextMenu, v:View, menuInfo:ContextMenu.ContextMenuInfo){
+         when(v.getId()){
+              R.id.dostij -> menu.add(0,MENU_APLHA_ID, 0, "alpha")
+              R.id.dostij -> menu.add(0,MENU_SCALE_ID, 0, "scale")
+             R.id.dostij -> menu.add(0,MENU_TRANSLATE_ID, 0, "translate")
+             R.id.dostij -> menu.add(0,MENU_ROTATE_ID, 0, "rotate")
+             R.id.dostij -> menu.add(0,MENU_COMBO_ID, 0, "combo")
+         }
+         super.onCreateContextMenu(menu, v, menuInfo)
+     }
+     val onContextItemSelected = { item: MenuItem ->
+         var anim = null as Animation
+         when(item.itemId){
+             MENU_APLHA_ID -> anim = AnimationUtils.loadAnimation(it, R.anim.myalpha)
+
+         }
+     }
+     */
 }
